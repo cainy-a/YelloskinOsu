1fcb90f0740f80fad001cc1d4bc699d85b8c8f4b
cainy-a <cainatkinson98@gmail.com>
2020-12-13 11:52:13 +0000

    Make build script overwrite existing osk file

--- buildskin.bat
+++ buildskin.bat
@@ -1,4 +1,5 @@
 @echo off
 echo This script requires 7-zip installed at C:\Program Files\7-Zip by the way
+del Yelloskin.osk
 "C:\Program Files\7-Zip\7z.exe" a out.zip .\current\
 rename out.zip Yelloskin.osk
