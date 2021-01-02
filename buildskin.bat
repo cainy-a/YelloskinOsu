d7d1f0368db48b4bcf78f4135c09f001ac2ed94e
cainy-a <cainy-a@users.noreply.github.com>
2021-01-02 01:32:21 +0000

    update build script

--- buildskin.bat
+++ buildskin.bat
@@ -2,4 +2,4 @@
 echo This script requires 7-zip installed at C:\Program Files\7-Zip by the way
 del Yelloskin.osk
 "C:\Program Files\7-Zip\7z.exe" a out.zip .\current\
-rename out.zip Yelloskin.osk
+rename out.zip Distraction-LESS.osk
