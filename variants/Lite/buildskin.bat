9d12912c4c20e4a528c6b926c8ee38b83e470429
cainy-a <cainy-a@users.noreply.github.com>
2021-01-02 11:55:42 +0000

    f

--- variants/Lite/buildskin.bat
+++ variants/Lite/buildskin.bat
@@ -2,4 +2,4 @@
 echo This script requires 7-zip installed at C:\Program Files\7-Zip by the way
 del Yelloskin.osk
 "C:\Program Files\7-Zip\7z.exe" a out.zip .\current\
-rename out.zip Distraction-LESS.osk
+rename out.zip Distraction-LESS-Lite.osk
