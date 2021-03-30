6e6fd991f6b6ee10fef3f2aa8a49fc6321be0b75
cainy-a <cainy-a@users.noreply.github.com>
2021-03-30 12:54:28 +0100

    New script nice

--- buildskin.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo This script requires 7-zip installed at C:\Program Files\7-Zip by the way
-del "Geometrix [Yellowsink Edit].osk"
-"C:\Program Files\7-Zip\7z.exe" a "Geometrix [Yellowsink Edit].osk" -tzip .\current\
---
+++ buildskin.py
@@ -0,0 +1,14 @@
+from zipfile import ZipFile
+import os
+from os.path import basename
+
+skinname = "Geometrix 「Yellowsink Edit」"
+
+with ZipFile(skinname + ".osk", 'w') as zipObj:
+   # Iterate over all the files in directory
+   for folderName, subfolders, filenames in os.walk("current"):
+       for filename in filenames:
+           #create complete filepath of file in directory
+           filePath = os.path.join(folderName, filename)
+           # Add file to zip
+           zipObj.write(filePath, skinname + "/" + basename(filePath))
